fe86a0534e071310fae519ea274af1c62da5ce76
Hoang Anh Triet <hat.info.tech@gmail.com>
2022-07-17 05:11:08 +0700

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@
 export const environment = {
   // Version structure s.m.f.b.e: Upgrade system . Add/Update module . Add/update feature . Build of upgrade/update . Environment
   version: '6.1.5.2',
+  // 6.1.5.2:   warehouse: expired and out of stock report anh notification
   // 6.1.5:     Dev sale, purchase, warehouse dashboard
   // 6.1.4:     Update pos search offline data
   // 6.1.3:     Update cost of goods sold calculate
